fb045a0b530441bc2692a71646a28a11a72a2128
wxp3967 <396763284@qq.com>
2017-07-19 15:58:51 +0800

    py init

---
+++ ProgrammingCollectiveIntelligence/chapter02/mycode.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 18:37:56 2017
+
+@author: Administrator
+"""
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 
+ 'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 
+ 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 
+ 'You, Me and Dupree': 3.5}, 
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+ 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+ 'The Night Listener': 4.5, 'Superman Returns': 4.0, 
+ 'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 
+ 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 2.0}, 
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+from math import sqrt
+#欧几里德距离评价
+#  （X1-X2）2 +(y1-y2）2  开根号 
+
+a=pow(2,3)
+n=sqrt(pow(4.5-4,2)+pow(1-2,2))
+
+
+r= 1/(1+n)
+
+def sim_distance(prefs,person1,person2):
+  # Get the list of shared_items
+  si={}
+  for item in prefs[person1]: 
+    if item in prefs[person2]: si[item]=1
+
+  # if they have no ratings in common, return 0
+  if len(si)==0: return 0
+
+  # Add up the squares of all the differences
+  sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) 
+                      for item in prefs[person1] if item in prefs[person2]])
+
+  return 1/(1+sum_of_squares)
+
+
+
+# 皮尔逊相关度评价
+
+def sim_pearson(prefs,p1,p2):
+  # Get the list of mutually rated items
+  si={}
+  for item in prefs[p1]: 
+    if item in prefs[p2]: si[item]=1
+
+  # if they are no ratings in common, return 0
+  if len(si)==0: return 0
+
+  # Sum calculations
+  n=len(si)
+  
+  # Sums of all the preferences
+  sum1=sum([prefs[p1][it] for it in si])
+  sum2=sum([prefs[p2][it] for it in si])
+  
+  # Sums of the squares
+  sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+  sum2Sq=sum([pow(prefs[p2][it],2) for it in si])	
+  
+  # Sum of the products
+  pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+  
+  # Calculate r (Pearson score)
+  num=pSum-(sum1*sum2/n)
+  den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+  if den==0: return 0
+
+  r=num/den
+
+  return r
+        
+#
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+
+    scores=[(similarity(prefs,person,other),other) for other in prefs if other!=person]
+    #sort() 函数用于对原列表进行排序，如果指定参数，则使用比较函数指定的比较函数。
+    scores.sort()
+    #reverse() 函数用于反向列表中元素。
+    scores.reverse()
+
+    return scores[0:n]
+
+
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
