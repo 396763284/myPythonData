cb1f6e2ae14d6f360599e24e6847d4314b63d134
wxp3967 <396763284@qq.com>
2017-07-31 19:24:28 +0800

    update padans

--- PythonForDataAnalysis/chapter_basic/basic_pandas.py
+++ PythonForDataAnalysis/chapter_basic/basic_pandas.py
@@ -5,4 +5,94 @@ Created on Mon Jul 31 16:41:40 2017
 @author: Administrator
 """
 
-#padnas 入门
+#pandas 入门
+from  pandas import Series,DataFrame
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#基础数据结构
+#series  带索引的数组 索引从0到N-1
+
+def function1():
+    obj=Series([1,7,-5,3])
+    print obj.values ,obj.index
+    obj2=Series([1,7,-5,3],index=['a','b','c','d'])
+    print obj2
+    #索引查看
+    print obj2['a']
+    #通过字典 创建
+    sdata={'ahio':10,'b':20}
+    obj3=Series(sdata)
+    print obj3
+    states=['ahio','b','a']
+    obj4=Series(sdata,index=states)
+    print obj4
+    # isnull  notnull
+    print pd.isnull(obj4)
+    print obj3+obj4
+    obj4.name='population'
+    obj4.index.name='state'
+    print obj4
+ 
+#function1()
+
+#DataFrame 表格型数据结构 类似值为数组的 json  {'a':[]}
+def function2():
+    data={'state':['a','b','c','d','e'],
+            'year':[2015,2016,2017,2018,2019],
+            'pop':[1.1,1.2,1.3,1.4,1.5]}
+    frame=DataFrame(data)
+    print frame
+    #索引
+    print frame['pop']
+
+
+
+
+# 基本功能 series DataFrame
+#算术运算和数据对齐
+def function3():
+     obj1=Series([1,7,-5,3],index=['a','b','c','d'])
+     #重置索引  插值处理
+     obj2=obj1.reindex(['b','c','d','r','a'],fill_value=0)
+    
+     print obj1+obj2
+     
+     #带重复值的轴索引
+     
+     
+     # 创建 dataframe
+     print np.arange(12).reshape((4,3))
+     df1=DataFrame(np.arange(12).reshape((4,3)),columns=list('bcd'),index=['a','b','c','d']) 
+     print df1
+     df2=DataFrame(np.arange(20).reshape((4,5)),columns=list('abcde'),index=['a','b','c','d']) 
+
+     print df1.add(df2,fill_value=0)
+
+
+#汇总和计算描述统计
+
+def function3():
+    df=DataFrame([[1.4,np.nan],[7.1,-4.5],[np.nan,np.nan],[0.75,-1.3]],index=['a','b','c','d'],columns=['one','two'])
+    print df
+    
+    
+    
+function3()    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
